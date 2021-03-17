b2faf287c099bbce55102c241a6ff8cd078292ef
Christian Steinmetzger <christian.steinmetzger@ki.se>
2021-03-17 20:04:15 +0100

    Added output to indicate how many structures were predicted and whether an energy or number threshold was used

--- mcff_submit.py
+++ mcff_submit.py
@@ -7,19 +7,13 @@
 # -------------------------------------------------------------------------------
 
 # Batch jobs can be submitted by using equal-length lists of directories, file names and sequences
-dir_name = ['/Users/chstei/Postdoc/E. coli ribosome/h23',
-            '/Users/chstei/Postdoc/E. coli ribosome/h23',
-            '/Users/chstei/Postdoc/E. coli ribosome/h23']
-file_name = ['h23-top_complete.out.txt',
-             'h23-top-GC_complete.out.txt',
-             'h23-top-GC-GC_complete.out.txt']
-nt_seq = ['GGUGUAGCGGUGAAAUGCGUAGAGACC',
-          'GUGUAGCGGUGAAAUGCGUAGAGAC',
-          'UGUAGCGGUGAAAUGCGUAGAGAC']
+dir_name = ['/Users/chstei/Postdoc/E. coli ribosome/h29']
+file_name = ['h29-ext8_complete.out.txt']
+nt_seq = ['GCGGUGGGCUUCGGCUGAAUCGC']
 
 # Specify one of these and set the other to None
-energy_cutoff = None
-predict_folds = 1000
+energy_cutoff = 8
+predict_folds = None
 
 # -------
 # Imports
@@ -39,18 +33,24 @@ for dir_name, file_name, nt_seq in zip(dir_name, file_name, nt_seq):
         with open(dir_name+'/'+file_name, 'w') as output_file:
             if predict_folds is None:
                 mcff_output = subprocess.check_output(f'mcff -s {nt_seq} -t {energy_cutoff}', shell=True, text=True)
+                report_string = f' structures within {energy_cutoff} kcal/mol'
             elif energy_cutoff is None:
                 # Requesting a defined number of output structures adds four lines to the top of the output that need to
                 # be removed before the file is written
                 mcff_output_raw = subprocess.check_output(f'mcff -s {nt_seq} -ft {predict_folds} -v2', shell=True, text=True)
                 mcff_output_cleaned = mcff_output_raw.splitlines(keepends=True)[4:]
                 mcff_output = ''.join(mcff_output_cleaned)
+                report_string = f'/{predict_folds} structures'
             else:
                 print('# ------------------------------------------------------------------------------------')
                 print('# Please request either an energy range or a number of suboptimal structures, not both')
                 print('# ------------------------------------------------------------------------------------')
                 break
             output_file.write(mcff_output)
+        output_size = str(mcff_output.count('\n'))
+        print('# ----------------------------' + '-' * (len(output_size) + len(report_string) + len(nt_seq)))
+        print(f'# MC-Flashfold predicted {output_size}{report_string} for {nt_seq}')
+        print('# ----------------------------' + '-' * (len(output_size) + len(report_string) + len(nt_seq)))
     except FileNotFoundError:
         print('# ----------------------------------------------------' + '-' * len(dir_name))
         print(f'# Please create the folder {dir_name} before running this script')
