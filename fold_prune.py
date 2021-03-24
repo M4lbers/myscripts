87a8429732283a62c53c5674b3c46fa28c11dc02
Christian Steinmetzger <christian.steinmetzger@ki.se>
2021-03-24 20:50:51 +0100

    Added option to filter the output for a specific base pair or bulge

--- fold_prune.py
+++ fold_prune.py
@@ -7,10 +7,12 @@
 # by an opened/closed base pair but have otherwise identical base pairing patterns
 # --------------------------------------------------------------------------------
 
-dir_names = ['/Users/chstei/Postdoc/E. coli ribosome/h23']
-file_names = ['h23-top_complete.out.txt']
+dir_names = ['/Users/chstei/Postdoc/E. coli ribosome/test']
+file_names = ['test_complete.out.txt']
 
-init = parse = make_table = single_bp = breathing = discard = write = 0
+filter_bp = (30, 0)
+
+init = parse = make_table = bp_filter = single_bp = breathing = discard = write = 0
 
 # -------
 # Imports
@@ -27,6 +29,7 @@ class TraceFile:
         self.dir_name = current_dir_name
         self.file_name = current_file_name
         self.raw_list = []
+        self.filter_list = []
         self.bridging_list = []
         self.breathing_list = []
         self.discard_list = []
@@ -46,12 +49,21 @@ class TraceFile:
             current_structure.append(bg.to_pair_table()[1:])                # Add bp table [3] and boolean flag for
             current_structure.append(bg.length_one_stem_basepairs() != [])  # single-bp-bridges [4] to the list
 
+    def filter_specific_bp(self):
+        for current_structure in self.raw_list:
+            print('***************')
+            print('Index: ' + str(current_structure[0]))
+            print(current_structure[1])
+            if filter_bp[0] - 1 not in current_structure[3] or current_structure[3][filter_bp[0] - 1] != filter_bp[1]:
+                self.filter_list.append(current_structure[0])
+                print('Requested base pair not present')
+
     def remove_single_bp_bridges(self):
         for current_structure in self.raw_list:
             print('***************')
             print('Index: ' + str(current_structure[0]))
             print(current_structure[1])
-            if current_structure[4]:        # The boolean flag is used to sort out any structure with a single-bp-stem
+            if current_structure[0] not in self.filter_list and current_structure[4]:        # The boolean flag is used to sort out any structure with a single-bp-stem
                 self.bridging_list.append(current_structure[0])
                 print('Single-basepair bridge')
 
@@ -59,12 +71,12 @@ class TraceFile:
         for current_structure in self.raw_list:
             print('***************')
             print('Index: ' + str(current_structure[0]))
-            if current_structure[0] not in self.bridging_list and current_structure[0] not in self.breathing_list:
+            if current_structure[0] not in self.filter_list and current_structure[0] not in self.bridging_list and current_structure[0] not in self.breathing_list:
                 print(current_structure[1])
                 for next_structure in self.raw_list[current_structure[0]:]:  # The index stored in line[0] for each line of raw_list starts at 1, so starting from slice [0] here automatically counts up by 1
                     print('---------------')
                     print('Next: ' + str(next_structure[0]))
-                    if next_structure[0] not in self.bridging_list and next_structure[0] not in self.breathing_list:
+                    if next_structure[0] not in self.filter_list and next_structure[0] not in self.bridging_list and next_structure[0] not in self.breathing_list:
                         print(next_structure[1])
                         for partner1, partner2 in zip(current_structure[3], next_structure[3]):
                             difference = abs(partner1 - partner2)
@@ -75,6 +87,8 @@ class TraceFile:
                         else:
                             self.breathing_list.append(next_structure[0])
                             print('Basepair breathing')
+                    elif next_structure[0] in self.filter_list:
+                        print('Skipped: Requested basepair not present')
                     elif next_structure[0] in self.bridging_list:
                         print('Skipped: Single-basepair bridge')
                     else:
@@ -86,14 +100,16 @@ class TraceFile:
         for current_structure in self.raw_list:
             print('***************')
             print('Index: ' + str(current_structure[0]))
-            if current_structure[0] in self.bridging_list or current_structure[0] in self.breathing_list:
+            if current_structure[0] in self.filter_list or current_structure[0] in self.bridging_list or current_structure[0] in self.breathing_list:
                 print('Skipped')
             else:
                 print(current_structure[1])
                 for next_structure in self.raw_list[current_structure[0]:]:  # The index stored in line[0] for each line of raw_list starts at 1, so starting from slice [0] here automatically counts up by 1
                     print('---------------')
                     print('Next: ' + str(next_structure[0]))
-                    if next_structure[0] in self.bridging_list:
+                    if next_structure[0] in self.filter_list:
+                        print('Skipped: Requested basepair not present')
+                    elif next_structure[0] in self.bridging_list:
                         print('Skipped: Single-basepair bridge')
                     elif next_structure[0] in self.breathing_list:
                         print('Skipped: Basepair breathing')
@@ -110,8 +126,8 @@ class TraceFile:
                             print('Basepair breathing')
 
     def discard_structures(self):
-        for line in sorted(self.bridging_list + self.breathing_list, reverse=True):
-            if line in self.bridging_list:
+        for line in sorted(self.filter_list + self.bridging_list + self.breathing_list, reverse=True):
+            if line in self.filter_list or line in self.bridging_list:
                 self.raw_list.pop(line - 1)
             else:
                 self.discard_list.append(self.raw_list.pop(line - 1))
@@ -138,6 +154,8 @@ for dir_name, file_name in zip(dir_names, file_names):
 # -----------------------
 # Apply filter algorithms
 # -----------------------
+    input_file.filter_specific_bp()
+    bp_filter = time.time()
     input_file.remove_single_bp_bridges()
     single_bp = time.time()
     input_file.var_remove_bp_breathing()
@@ -232,7 +250,8 @@ print(f'\nFinished at {time.ctime(finish)}')
 print(f'Init time: {round(init - start, 3)} s')
 print(f'Parse time: {round(parse - init, 3)} s')
 print(f'Table time: {round(make_table - parse, 3)} s')
-print(f'Single time: {round(single_bp - make_table, 3)} s')
+print(f'Filter time: {round(bp_filter - make_table, 3)} s')
+print(f'Single time: {round(single_bp - bp_filter, 3)} s')
 print(f'Breathing time: {round(breathing - single_bp, 3)} s')
 print(f'Discard time: {round(discard - breathing, 3)} s')
 print(f'Write time: {round(write - discard, 3)} s')
