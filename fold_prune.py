8c30bf39ed54041f32a9aa05b093b68e01a6c1e6
Christian Steinmetzger <christian.steinmetzger@ki.se>
2021-03-26 15:10:03 +0100

    Extended the functionality of the base pair filter to allow both inclusion and exclusion. Removed legacy code snippets

--- fold_prune.py
+++ fold_prune.py
@@ -11,9 +11,12 @@
 dir_names = ['/Users/chstei/Postdoc/E. coli ribosome/test']
 file_names = ['test_complete.out.txt']
 
-# Only keep structures that contain a specific base pair or bulged nucleotide?
-# Default: None, specify a base pair between nucleotides i and j as [(i, j)] or a bulge at nucleotide k as [(k, 0)]
-filter_bp = [(30, 14)]
+# Only keep structures that include or not include specific base pairs or bulged nucleotides?
+# Default: None, specify a base pair between nucleotides i and j as [(i, j)] or a bulge at nucleotide k as [(k, 0)].
+# Multiple criteria can be given as, e.g., [(i, j), (k, 0)]
+filter_bp = [(14, 30), (21, 24)]
+# Default: 'include' to keep only structures that contain all of the requested base pair patterns. Set to 'exclude'
+filter_mode = 'include'
 
 init = parse = make_table = bp_filter = single_bp = breathing = discard = write = 0
 
@@ -58,8 +61,7 @@ class TraceFile:
             print('Index: ' + str(current_structure[0]))
             print(current_structure[1])
             for entry in filter_bp:
-                # if entry[0] not in current_structure[3] or current_structure[3][entry[0] - 1] != entry[1]:
-                if current_structure[3][entry[0] - 1] != entry[1]:
+                if (filter_mode == 'include' and current_structure[3][entry[0] - 1] != entry[1]) or (filter_mode == 'exclude' and current_structure[3][entry[0] - 1] == entry[1]):
                     self.filter_list.append(current_structure[0])
                     print('Requested base pair not present')
                     break
@@ -74,35 +76,6 @@ class TraceFile:
                 print('1-bp bridge')
 
     def remove_bp_breathing(self):
-        for current_structure in self.raw_list:
-            print('***************')
-            print('Index: ' + str(current_structure[0]))
-            if current_structure[0] not in self.filter_list and current_structure[0] not in self.bridging_list and current_structure[0] not in self.breathing_list:
-                print(current_structure[1])
-                for next_structure in self.raw_list[current_structure[0]:]:  # The index stored in line[0] for each line of raw_list starts at 1, so starting from slice [0] here automatically counts up by 1
-                    print('---------------')
-                    print('Next: ' + str(next_structure[0]))
-                    if next_structure[0] not in self.filter_list and next_structure[0] not in self.bridging_list and next_structure[0] not in self.breathing_list:
-                        print(next_structure[1])
-                        for partner1, partner2 in zip(current_structure[3], next_structure[3]):
-                            difference = abs(partner1 - partner2)
-                            print(partner1, partner2, difference)
-                            if difference != 0 and difference != max(partner1, partner2):
-                                print('Different fold')
-                                break
-                        else:
-                            self.breathing_list.append(next_structure[0])
-                            print('Base pair breathing')
-                    elif next_structure[0] in self.filter_list:
-                        print('Skipped: Requested base pair not present')
-                    elif next_structure[0] in self.bridging_list:
-                        print('Skipped: 1-bp bridge')
-                    else:
-                        print('Skipped: Base pair breathing')
-            else:
-                print('Skipped')
-
-    def var_remove_bp_breathing(self):  # This implementation should do the same and might be slightly faster
         for current_structure in self.raw_list:
             print('***************')
             print('Index: ' + str(current_structure[0]))
@@ -139,10 +112,13 @@ class TraceFile:
                 self.discard_list.append(self.raw_list.pop(line - 1))
 
     def write_output(self):
-        with open(self.dir_name+'/'+self.file_name.replace('complete', 'filtered_30-14_var'), 'w') as pruned_file:
+        with open(self.dir_name+'/'+self.file_name.replace('complete', 'incl_14-30_21-24'), 'w') as pruned_file:
             for line in self.raw_list:
                 pruned_file.write(f'{line[1]} {line[2]}\n')
 
+
+if filter_mode not in ['include', 'exclude']:
+    raise ValueError('Check that the filter mode has been set correctly')
 
 start = time.time()
 print(f'Started at {time.ctime(start)}\n')
@@ -166,7 +142,7 @@ for dir_name, file_name in zip(dir_names, file_names):
         bp_filter = time.time()
     input_file.remove_single_bp_bridges()
     single_bp = time.time()
-    input_file.var_remove_bp_breathing()
+    input_file.remove_bp_breathing()
     breathing = time.time()
     input_file.discard_structures()
     discard = time.time()
@@ -179,13 +155,13 @@ for dir_name, file_name in zip(dir_names, file_names):
 finish = time.time()
 print(f'\nFinished at {time.ctime(finish)}')
 
-# print(f'Init time: {round(init - start, 3)} s')
-# print(f'Parse time: {round(parse - init, 3)} s')
-# print(f'Table time: {round(make_table - parse, 3)} s')
-# print(f'Filter time: {round(bp_filter - make_table, 3)} s')
-# print(f'Single time: {round(single_bp - bp_filter, 3)} s')
-# print(f'Breathing time: {round(breathing - single_bp, 3)} s')
-# print(f'Discard time: {round(discard - breathing, 3)} s')
-# print(f'Write time: {round(write - discard, 3)} s')
+print(f'Init time: {round(init - start, 3)} s')
+print(f'Parse time: {round(parse - init, 3)} s')
+print(f'Table time: {round(make_table - parse, 3)} s')
+print(f'Filter time: {round(bp_filter - make_table, 3)} s')
+print(f'Single time: {round(single_bp - bp_filter, 3)} s')
+print(f'Breathing time: {round(breathing - single_bp, 3)} s')
+print(f'Discard time: {round(discard - breathing, 3)} s')
+print(f'Write time: {round(write - discard, 3)} s')
 
 print(f'Elapsed time: {round(finish - start, 3)} s')
